28d799009dcc06bc1dfde0da833b5272e723d4c2
lamhuynh <lamhuynhb778@gmail.com>
2021-04-02 14:26:37 +0700

    demo read & write file

---
+++ File/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System;
+
+namespace File
+{
+    enum Options
+    {
+        y,
+        n
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your file name: ");
+            string fileName = Console.ReadLine();
+            string extension = ".txt";
+            string path = "./file/";
+            string filePath = path + fileName + extension;
+
+            if (!isExistingFile(filePath)) {
+                Console.WriteLine("Enter your content: ");
+                string content = Console.ReadLine();
+                System.IO.File.WriteAllText(filePath, content + Environment.NewLine);
+            } else {
+                Console.WriteLine($"File {filePath} is existing!!!");
+            }
+            
+            string option;
+            do
+            {
+                Console.WriteLine("Do you want to append more content (y/n)?");
+                option = Console.ReadLine();
+
+                if (isYes(option)) {
+                    Console.WriteLine("Please enter your content: ");
+                    string appendContent = Console.ReadLine();
+                    System.IO.File.AppendAllText(filePath, appendContent + Environment.NewLine);
+                }
+
+            } while (isYes(option));
+
+            Console.WriteLine(System.IO.File.ReadAllText(filePath));
+        }
+
+        static bool isYes(string option)
+        {
+            return option == Options.y.ToString();
+        }
+
+        static bool isExistingFile(string filePath)
+        {
+            return System.IO.File.Exists(filePath);
+        }
+
+    }
+}
